aff91aeb0c5c8ac4b3107fe9c0192a2e0f872f94
Stefan Ilic <stefan.ilic.vie@gmail.com>
2018-10-05 22:23:35 +0200

    Add enum for basic sets

---
+++ Interfaces/Set.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interfaces
+{
+    public enum Set
+    {
+        N,
+        Z,
+        R,
+        Q
+    }
+}
