cfd4bfb2303aa3e7037ef73f1482b80a402cb192
Stefan Ilic <stefan.ilic.vie@gmail.com>
2018-10-05 22:24:32 +0200

    Add Interfaces for common math structures

---
+++ Interfaces/IGroup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interfaces
+{
+    public interface IGroup<out T>
+    {
+        int Order { get; }
+        bool IsCyclic { get; }
+        bool IsSymmetric { get; }
+        IEnumerable<T> Generators { get; }
+        IEnumerable<IGroup<T>> SubGroups { get; }
+        Operation Operation { get; }
+        IEnumerable<T> Elements { get; }
+    }
+}
---
+++ Interfaces/IPolynomial.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interfaces
+{
+    public interface IPolynomial
+    {
+        IField Field { get; }
+        int Degree { get; }
+        string HexadecimalRepresentation { get; }
+        string BinaryRepresentation { get; }
+    }
+}
